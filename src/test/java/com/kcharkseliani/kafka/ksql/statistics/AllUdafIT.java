18d699c9fe2cebe552da5800db9ffe51479c27c7
kcharkseliani <charkseliani.k@gmail.com>
2025-05-05 20:01:22 +0100

    Added bodies for startContainers and stopContainers methods that spin up kafka and ksqlDB

--- src/test/java/com/kcharkseliani/kafka/ksql/statistics/AllUdafIT.java
+++ src/test/java/com/kcharkseliani/kafka/ksql/statistics/AllUdafIT.java
@@ -2,14 +2,37 @@ package com.kcharkseliani.kafka.ksql.statistics;
 
 import org.junit.jupiter.api.*;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.Network;
 
 public class AllUdafIT {
 
+    static KafkaContainer kafka;
+    static GenericContainer<?> ksqldb;
+
+    static Network network = Network.newNetwork();
+
     @BeforeAll
     static void startContainers() {
-        throw new UnsupportedOperationException("Method startContainers not implemented");
+        kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.8.0"))
+        .withNetwork(network)
+        .withNetworkAliases("kafka")
+        .withEnv("KAFKA_ADVERTISED_LISTENERS", "PLAINTEXT://kafka:9092")
+        .withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9092");
+
+        kafka.start();
+
+        ksqldb = new GenericContainer<>(DockerImageName.parse("confluentinc/ksqldb-server:0.29.0"))
+            .withNetwork(network)
+            .withExposedPorts(8088)
+            .withEnv("KSQL_BOOTSTRAP_SERVERS", "PLAINTEXT://kafka:9092")
+            .withEnv("KSQL_KSQL_EXTENSION_DIR", "/opt/ksqldb-udfs")  // mount your UDAF JAR here
+            .withFileSystemBind("extensions", "/opt/ksqldb-udfs", BindMode.READ_ONLY) // mounts your UDAF uber-jar
+            .dependsOn(kafka);
+
+        ksqldb.start();
     }
 
     @Test
@@ -19,7 +42,7 @@ public class AllUdafIT {
 
     @AfterAll
     static void stopContainers() {
-        throw new UnsupportedOperationException("Method stopContainers not implemented");
+        ksqldb.stop();
+        kafka.stop();
     }
-
 }
