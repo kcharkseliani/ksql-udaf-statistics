5837b6e3394749f3360fdb2dbab4ef3783c35608
kcharkseliani <charkseliani.k@gmail.com>
2025-05-05 16:36:03 +0100

    Added required Testcontainers dependencies, created integrationTest and verify tasks, added empty AllUdafIT test class

---
+++ src/test/java/com/kcharkseliani/kafka/ksql/statistics/AllUdafIT.java
@@ -0,0 +1,25 @@
+package com.kcharkseliani.kafka.ksql.statistics;
+
+import org.junit.jupiter.api.*;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class AllUdafIT {
+
+    @BeforeAll
+    static void startContainers() {
+        throw new UnsupportedOperationException("Method startContainers not implemented");
+    }
+
+    @Test
+    void testKsqlWithCustomUdaf() throws Exception {
+        throw new UnsupportedOperationException("Method testKsqlWithCustomUdaf not implemented");
+    }
+
+    @AfterAll
+    static void stopContainers() {
+        throw new UnsupportedOperationException("Method stopContainers not implemented");
+    }
+
+}
